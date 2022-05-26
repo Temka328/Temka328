da7af0703cd8439088c543ee2a4ef78aa44490ef
Artem Astafurov <artem.astafurov0899@gmail.com>
2022-05-26 17:31:51 +0600

    kt

--- src/Exceptions/IncorrectCommandException.cpp
+++
@@ -1,6 +0,0 @@
-#include "../headers/Exceptions/IncorrectCommandException.h"
-
-const char *IncorrectCommandException::what() const noexcept
-{
-    return message.c_str();
-};
--- src/Exceptions/ObjectPropertyException.cpp
+++
@@ -1,6 +0,0 @@
-#include "../headers/Exceptions/ObjectPropertyException.h"
-
-const char *ObjectPropertyException::what() const noexcept
-{
-    return message.c_str();
-};
